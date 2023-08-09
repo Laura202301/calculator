5f4c74fabcc76c50b19222a215869d202dfae3a2
Laura202301 <fedele.laura17@gmail.com>
2023-08-08 22:29:46 -0300

    Implement calculator functionality (click event)

--- javascript.js
+++ javascript.js
@@ -1,32 +1,101 @@
-let firstNumber = 0;
-let SecondNumber = 0;
+// Initialize variables to store numbers, operator, and display value
+let firstNumber = 0; 
+let secondNumber = 0;
 let operator = "";
 let displayValue = 0;
 
-const add = function (firstNumber, SecondNumber) {
-  return firstNumber + SecondNumber;
+// Functions for basic arithmetic operations
+const add = function (firstNum, secondNum) {
+  return firstNum + secondNum;
 };
 
-const subtract = function (firstNumber, SecondNumber) {
-  return firstNumber - SecondNumber;
+const subtract = function (firstNum, secondNum) {
+  return firstNum - secondNum;
 };
 
-const multiply = function (firstNumber, SecondNumber) {
-  return firstNumber * SecondNumber;
+const multiply = function (firstNum, secondNum) {
+  return firstNum * secondNum;
 };
 
-const divide = function (firstNumber, SecondNumber) {
-  return firstNumber/ SecondNumber;
-};
-
-function operate (firstNumber, SecondNumber, operator)
-  if (operator === "+") {
-    operate = add (firstNumber, SecondNumber);
-  }else if (operator === "-") {
-    operate = subtract (firstNumber, SecondNumber);
-  }else if (operator === "*") {
-    operate = multiply (firstNumber, SecondNumber);
-  }else if (operator === "/"); {
-    operate = divide(firstNumber, SecondNumber);
+const divide = function (firstNum, secondNum) {
+  if (secondNum === 0) {
+    return "Error"; // Handle division by zero
   }
+  return firstNum / secondNum;
+};
 
+// Function to perform the selected operation
+function operate(firstNum, secondNum, operator) {
+  if (operator === "+") {
+    return add(firstNum, secondNum);
+  } else if (operator === "-") {
+    return subtract(firstNum, secondNum);
+  } else if (operator === "x") {
+    return multiply(firstNum, secondNum);
+  } else if (operator === "/") {
+    return divide(firstNum, secondNum);
+  }
+}
+
+// Set up event listeners for button clicks
+const buttons = document.querySelectorAll('.buttons button'); // Get all buttons
+const calculatorDisplay = document.getElementById('calculatorDisplay'); // Display for numbers
+const operationDisplay = document.getElementById('operationDisplay'); // Display for operations
+
+buttons.forEach(button => {
+  button.addEventListener('click', function () {
+    const buttonText = button.textContent;
+    
+    // Handling number buttons
+    if (buttonText >= '0' && buttonText <= '9') {
+      if (operator === "") {
+        firstNumber = parseFloat(calculatorDisplay.textContent + buttonText);
+        displayValue = firstNumber;
+      } else {
+        secondNumber = parseFloat(calculatorDisplay.textContent + buttonText);
+        displayValue = secondNumber;
+      }
+    } 
+    
+    // Handling operator buttons
+    else if (buttonText === "+" || buttonText === "-" || buttonText === "x" || buttonText === "/") {
+      operator = buttonText;
+      displayValue = 0;
+      operationDisplay.textContent = firstNumber + " " + operator;
+    } 
+    
+    // Handling the equal button
+    else if (buttonText === "=") {
+      if (operator !== "" && secondNumber !== 0) {
+        const result = operate(firstNumber, secondNumber, operator);
+        displayValue = result;
+        firstNumber = result;
+        secondNumber = 0;
+        operator = "";
+        operationDisplay.textContent = "";
+      }
+    } 
+    
+    // Handling the Clear button
+    else if (buttonText === "Clear") {
+      firstNumber = 0;
+      secondNumber = 0;
+      operator = "";
+      displayValue = 0;
+      operationDisplay.textContent = "";
+    } 
+    
+    // Handling the backspace button
+    else if (buttonText === "\u232B") {
+      if (operator === "") {
+        firstNumber = Math.floor(firstNumber / 10);
+        displayValue = firstNumber;
+      } else {
+        secondNumber = Math.floor(secondNumber / 10);
+        displayValue = secondNumber;
+      }
+    }
+    
+    calculatorDisplay.textContent = displayValue; // Update the displayed value
+  });
+});
