c5fbb566ea6e1fff62e6b869485845dabc3d3d52
Laura202301 <fedele.laura17@gmail.com>
2023-08-11 19:35:40 -0300

    Implement highlight functionality for keydown-keyup events

--- javascript.js
+++ javascript.js
@@ -5,6 +5,15 @@ let operator = "";
 let displayValue = 0;
 let decimalPressed = false; // variable to track decimal point
 
+// Map special keys with their code equivalents
+const specialKeys = {
+  "Backspace": "\u232B",
+  "Delete": "Clear",
+  "Enter": "=",
+  "m": "+/-",
+  "*": "x" 
+};
+
 // Functions for basic arithmetic operations
 const add = function (firstNum, secondNum) {
   return firstNum + secondNum;
@@ -39,9 +48,16 @@ function operate(firstNum, secondNum, operator) {
 }
 
 // Set up event listeners for button clicks
-const buttons = document.querySelectorAll('.buttons button'); // Get all buttons
-const calculatorDisplay = document.getElementById('calculatorDisplay'); // Display for numbers
-const operationDisplay = document.getElementById('operationDisplay'); // Display for operations
+// Get all buttons
+const buttons = document.querySelectorAll('.buttons button'); 
+// Display for numbers
+const calculatorDisplay = document.getElementById('calculatorDisplay'); 
+// Display for operations
+const operationDisplay = document.getElementById('operationDisplay');
+
+// Set up event listeners for keydown and keyup events
+document.addEventListener('keydown', handleKeyDown);
+document.addEventListener('keyup', handleKeyUp);
 
 buttons.forEach(button => {
   button.addEventListener('click', handleButtonClick);
@@ -53,11 +69,35 @@ function handleButtonClick() {
   handleInput(buttonText);
 }
 
-// Function to handle keyboard input
-document.addEventListener('keydown', function(event) {
+// Function to handle keydown event
+function handleKeyDown(event) {
   const key = event.key;
-  handleInput(key);
-});
+  highlightKey(specialKeys[key] || key, true); // Highlight pressed key
+  handleInput(specialKeys[key] || key);
+}
+
+// Function to handle keyup event
+function handleKeyUp(event) {
+  const key = event.key;
+  highlightKey(specialKeys[key] || key, false); // Deactivate highlight 
+}
+
+// Function to highlight or unhighlight a key
+function highlightKey(key, isHighlighted) {
+  const button = getButtonByKey(key);
+  if (button) {
+    if (isHighlighted) {
+      button.classList.add('highlighted');
+    } else {
+      button.classList.remove('highlighted');
+    }
+  }
+}
+
+// Function to get the button corresponding to a key
+function getButtonByKey(key) {
+  return [...buttons].find(button => button.textContent === key);
+}
 
 // Function to handle both button clicks and keyboard input
 function handleInput(input) {
@@ -151,3 +191,8 @@ function handleInput(input) {
 
   calculatorDisplay.textContent = displayValue; // Update the displayed value
 }
+
+// Add event listener to footer icon
+document.querySelector(".icon").addEventListener("click", function() {
+  window.open("https://github.com/Laura202301/calculator", "_blank");
+});
